2ee4f1da34fefae54880c329c01a8b7ff77c6311
hawra al ramadan <al.ramdan.hawra@gmail.com>
2023-02-06 21:14:44 +0300

    adding style and function

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let firstCard = 6 ;
-let secondCard = 9 ;
+let firstCard = 10 ;
+let secondCard = 11 ;
 let sum = firstCard + secondCard ;
 let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl= document.getElementById("message-el");
 
-if (sum <= 20){
-    console.log("Do you want to draw a new card? 🙂");
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳");
-} else {
-    console.log("You're out of the game! 😭");
+ function startGame(){
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+    } else {
+        message = "You're out of the game!"
+    }
+     messageEl.textContent = message;
 }
 
-//CASH OUT!
-console.log(hasBlackJack);
