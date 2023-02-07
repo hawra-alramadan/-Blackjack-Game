b09f8599d80ca3acd3b8d21b190f787774ccb387
hawra al ramadan <al.ramdan.hawra@gmail.com>
2023-02-07 20:59:42 +0300

    add sum and cards

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl= document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
 
  function startGame(){
+    sumEl.textContent = "Sum: " + sum;
+    cardEl.textContent ="Cards: " + firstCard +" "+ secondCard;
     if (sum <= 20){
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -17,3 +21,7 @@ let messageEl= document.getElementById("message-el");
      messageEl.textContent = message;
 }
 
+
+function newGame(){
+
+}
