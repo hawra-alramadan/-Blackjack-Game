1d18b8dcb36e521217123fda96ae76f2eee79e86
hawra al ramadan <al.ramdan.hawra@gmail.com>
2023-05-09 23:29:31 +0300

    Random card if

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-let firstCard = getRandomCard() ;
-let secondCard = getRandomCard() ;
-let cards = [firstCard, secondCard] //arry
-let sum = firstCard + secondCard ;
+let cards = [] //arry
+let sum = 0 ;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl= document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.getElementById("card-el");
 
 function getRandomCard(){
-    let s = Math.floor(Math.random() * 11)
-    return s
+    let randomNumber = Math.floor(Math.random()* 13) + 1 ;
+    if (randomNumber > 10){
+        return 10
+    } else if (randomNumber === 1){
+        return 11
+    } else {
+        return randomNumber
+    }
+    
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -44,5 +54,5 @@ function newCard(){
     messageEl.textContent = message;
     //Push the card to the cards array
     cards.push(card)
-    startGame()
+    renderGame()
 }
